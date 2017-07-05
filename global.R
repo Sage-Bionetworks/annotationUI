100942d4c6f69264a9e8e137ad320e5ac20001bb
teslajoy <nasim@plenary.org>
2017-07-05 15:40:22 -0700

    grabs the latest release version

--- global.R
+++ global.R
@@ -52,5 +52,6 @@ all.vars <- names(dat)
 names(dat) <- c("key", "description", "columnType", "maximumSize", "value", "valueDescription", "source", "project")
 
 # Render latest github release version in title from synapseAnnotations repo
-release.version <- gsub(".*/", "", system("git ls-remote --tags https://github.com/Sage-Bionetworks/synapseAnnotations", intern = TRUE)[[4]])
+versions <- system("git ls-remote --tags https://github.com/Sage-Bionetworks/synapseAnnotations", intern = TRUE)
+release.version <- gsub(".*/", "", versions[[length(versions)]])
 
